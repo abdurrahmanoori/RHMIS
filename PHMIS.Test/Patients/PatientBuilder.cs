6246211a536b446e00f84579f78b061ffb959b99
Abdurrahman <Abdurrahmannoori10@gmail.com>
2025-09-09 23:15:38 +0430

    half integraiton test implemened

---
+++ PHMIS.Test/Patients/PatientBuilder.cs
@@ -0,0 +1,39 @@
+﻿using PHMIS.Application.DTO.Patients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHMIS.Test.Patients
+{
+    public class PatientBuilder
+    {
+        private string _firstName = "John";
+        private string _lastName = "Doe";
+        private string _email = $"john.doe_{Guid.NewGuid():N}@test.com";
+
+        public PatientBuilder WithFirstName(string firstName)
+        {
+            _firstName = firstName;
+            return this;
+        }
+
+        public PatientBuilder WithEmail(string email)
+        {
+            _email = email;
+            return this;
+        }
+
+        public PatientCreateDto BuildCreateDto( ) => new()
+        {
+            FirstName = _firstName,
+            LastName = _lastName,
+            DateOfBirth = new DateTime(1990, 1, 1),
+            Gender = "Male",
+            PhoneNumber = "123-456-7890",
+            Email = _email,
+            Address = "123 Test St"
+        };
+    }
+}
---
+++ PHMIS.Test/TestInfrastructure/CleanupDatabaseAsync.cs
@@ -0,0 +1,14 @@
+﻿using PHMIS.Infrastructure.Context;
+
+namespace PHMIS.Test.TestInfrastructure
+{
+    public async Task CleanupDatabaseAsync( )
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        // Clear specific tables to avoid FK issues
+        db.Patients.RemoveRange(db.Patients);
+        await db.SaveChangesAsync();
+    }
+}
