175a6a3f11af859e941161b49c6193af39d61848
ARD-Developer <abdurrahmannoori10@gmail.com>
2025-09-09 08:57:23 +0430

    domain and application stuff added with error

---
+++ RHMIS.Application/Extentions/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+
+namespace RHMIS.Application.Extentions;
+
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection ConfigureApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+
+
+            return services;
+        }
+    }
