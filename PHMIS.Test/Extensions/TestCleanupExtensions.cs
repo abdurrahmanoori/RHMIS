1228f449950cea73994dd603be7098b25aba439e
ARD-Developer <abdurrahmannoori10@gmail.com>
2025-09-10 17:00:49 +0430

    ************** ALL CURD OPERATION FOR PATEINT COMPLETED WITH INEGRATION TESTS**************

---
+++ PHMIS.Test/Extensions/TestCleanupExtensions.cs
@@ -0,0 +1,19 @@
+﻿using PHMIS.Infrastructure.Context;
+using PHMIS.Test.TestInfrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace PHMIS.Test.Extensions
+{
+    public static class TestCleanupExtensions
+    {
+        public static async Task CleanupPatientsAsync(this CustomWebApplicationFactory factory)
+        {
+            using var scope = factory.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            dbContext.Patients.RemoveRange(dbContext.Patients);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
