833d3959516ddac8649d3f500518792f16a97f05
Abdurrahman <Abdurrahmannoori10@gmail.com>
2025-09-10 22:36:21 +0430

    Implement BaseEntity and refactor related classes
    Created `BaseEntity` class with `Id` and `Name` properties. Updated `LabTest`, `LabTestGroup`, and `Province` classes to inherit from `BaseEntity`, removing their own `Id` and `Name` properties. Adjusted database files to reflect these structural changes, including the deletion of `.shm` and `.wal` files.

---
+++ PHMIS.Domain/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PHMIS.Domain.Common
+{
+    public abstract class BaseEntity 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
--- PHMIS.Domain/Entities/Laboratory/LabTestGroup.cs
+++ PHMIS.Domain/Entities/Laboratory/LabTestGroup.cs
@@ -1,10 +1,9 @@
-﻿
+﻿using PHMIS.Domain.Common;
+
 namespace PHMIS.Domain.Entities.Laboratory
 {
-    public class LabTestGroup
+    public class LabTestGroup : BaseEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
         public string? Description { get; set; }
         public short? SortOrder { get; set; }
         public ICollection<LabTest> LabTests { get; set; }
